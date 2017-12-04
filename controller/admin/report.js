560eb56fc3a9f132b71ccc1a9680ef7a2dba20f0
food869 <food869@naver.com>
2017-12-04 21:39:28 +0900

    admin report

--- controller/admin/report.js
+++ controller/admin/report.js
@@ -5,7 +5,7 @@ exports.report_list=(req,res)=>{
   if(req.user.User_isAdmin===0){
     return res.render('main',{message:"관리자만 접속할 수 있습니다."})
   }else {
-    db.query('select * from report', (err, results) => {
+    db.query('select user.User_Name, report.* from user,comment,report where user.User_ID=comment.User_ID and comment.Comment_ID=report.Comment_ID;', (err, results) => {
       if (err) console.log(err);
       res.render('admin/report/report', {report: results});
     })
@@ -13,7 +13,7 @@ exports.report_list=(req,res)=>{
 };
 exports.report_view=(req,res)=>{
   const report_id=req.params.id;
-  db.query('select * from report where Report_ID=?',[report_id],(err,results)=>{
+  db.query('select user.User_Name, report.* from user,comment,report where user.User_ID=comment.User_ID and comment.Comment_ID=report.Comment_ID and report.Report_ID=?',[report_id],(err,results)=>{
     if(err) console.log(err);
     res.render('admin/report/report_view',{report:results});
   })
