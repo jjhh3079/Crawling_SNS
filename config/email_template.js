bb7a85eefa3a85af860295407eda84aaf6d379bd
jeonghoj <34781828+jeonghoj@users.noreply.github.com>
2019-09-07 19:24:50 +0900

    모듈 업데이트 및 이메일 템플릿 업데이트

--- config/email_template.js
+++ config/email_template.js
@@ -1,6 +1,6 @@
 exports.email_verify = (email,token) =>{
   return {
-    from: 'jjhh3079@gmail.com',
+    from: 'CrawlingSNS@gmail.com',
     to : email,
     subject: "Crawling SNS 이메일을 인증해주세요",
     text : "링크를 클릭하여 인증해주세요 : " + "http://192.9.44.53:65005/email_verify?token=" + token
@@ -8,7 +8,7 @@ exports.email_verify = (email,token) =>{
 };
 exports.find_pw = (email,token) =>{
   return {
-    from: 'jjhh3079@gmail.com',
+    from: 'CrawlingSNS@gmail.com',
     to : email,
     subject: "Crawling SNS 이메일을 인증해주세요",
     text : "비밀번호를 다시 설정하려면 링크를 클릭하세요 : " + "http://192.9.44.53:65005/new_pw?token=" + token
@@ -16,7 +16,7 @@ exports.find_pw = (email,token) =>{
 };
 exports.eleven=(email)=>{
   return {
-    from: 'jjhh3079@gmail.com',
+    from: 'CrawlingSNS@gmail.com',
     to : email,
     subject: "11번가에서 새로운 이벤트가 생겼어요!",
     text : "빨리 이벤트를 확인해보세요! http://192.9.44.53:65005"
@@ -24,7 +24,7 @@ exports.eleven=(email)=>{
 };
 exports.tmon=(email)=>{
   return {
-    from: 'jjhh3079@gmail.com',
+    from: 'CrawlingSNS@gmail.com',
     to : email,
     subject: "티몬에서 새로운 이벤트가 생겼어요!",
     text : "빨리 이벤트를 확인해보세요! http://192.9.44.53:65005"
@@ -32,7 +32,7 @@ exports.tmon=(email)=>{
 };
 exports.ppomppu=(email)=>{
   return {
-    from: 'jjhh3079@gmail.com',
+    from: 'CrawlingSNS@gmail.com',
     to : email,
     subject: "뽐뿌에서 새로운 핫딜이 생겼어요!",
     text : "빨리 핫딜을 확인해보세요! http://192.9.44.53:65005"
@@ -40,7 +40,7 @@ exports.ppomppu=(email)=>{
 };
 exports.coolenjoy=(email)=>{
   return {
-    from: 'jjhh3079@gmail.com',
+    from: 'CrawlingSNS@gmail.com',
     to : email,
     subject: "쿨엔조이에서 새로운 핫딜이 생겼어요!",
     text : "빨리 핫딜을 확인해보세요! http://192.9.44.53:65005"
